e8f6cf9a8c1875b4a435865374aad03d9789c4d8
Victor Ivan Saa Caicedo <vis97c@hotmail.com>
2020-09-11 16:09:41 -0500

    functionality fix

--- src/js/App.vue
+++ src/js/App.vue
@@ -5,33 +5,40 @@
 			<img src="/images/shapes.png" alt="" class="m__bgColor-none" />
 		</div>
 
-		<div class="c-holder gm__txtAlign-center">
-			<div v-if="!playing" class="c-flxInline m__flxColumn gu__flx-center">
-				<div class="x-txt xm__txtAlign-center">
-					<p>
-						<b>Hello friend, tell me your name...</b>
-					</p>
+		<div class="c-holder gm__txtAlign-center gm__hidden:md-inv">
+			<div v-if="landing" class="c-flxInline m__flxColumn gu__flx-center">
+				<!-- landing -->
+				<div v-if="!submited" class="c-flxInline m__flxColumn gu__flx-center">
+					<div class="x-txt xm__txtAlign-center">
+						<p>
+							<b>Hello friend, tell me your name...</b>
+						</p>
+					</div>
+					<input v-model="name" type="text" class="c-inTxt" placeholder="Your name here" />
 				</div>
-				<input v-model="name" type="text" class="c-inTxt" placeholder="Your name here" />
+				<!-- score -->
 				<div v-if="timeLeft == 25 || timeLeft == null" class="c-flx m__flxRow gu__flx-center">
-					<Trigger @click="start" />
+					<Trigger @click="toGame" />
 				</div>
-
-				<div v-if="timeLeft == 0" class="c-flx m__flxRow gu__flx-center">
-					<p class="gm__txtColor-primary">
-						<b>You ran out of time</b>
-					</p>
-					<Trigger msg="Start over" @click="start" />
+				<div v-if="timeLeft <= 0 && timeLeft != null" class="c-flx m__flxColumn gu__flx-center">
+					<div class="c-flx m__flxRow gu__flx-center">
+						<p class="gm__txtColor-primary">
+							<b>You ran out of time</b>
+						</p>
+						<Trigger msg="Start over" @click="toGame" />
+					</div>
+					<p class="gm__txtSize-xs gm__txtColor-dark4">Restarting in a few seconds...</p>
 				</div>
 				<div v-if="timeLeft > 0 && full == 5" class="c-flx m__flxColumn gu__flx-center">
 					<div class="c-flx m__flxRow gu__flx-center">
 						<p class="gm__txtColor-secondary">
 							<b>Congratulations, you won!</b>
 						</p>
-						<Trigger msg="Play again" @click="start" />
+						<Trigger msg="Play again" @click="toGame" />
 					</div>
-					<p class="gm__txtSize-xs gm__txtColor-dark3">
-						<b>Your score: {{ timeLeft }}</b>
+					<p class="gm__txtSize-xs gm__txtColor-dark4">
+						<b class="gm__txtColor-dark9">Your score: {{ timeLeft }}</b>
+						, restarting in a few seconds...
 					</p>
 				</div>
 			</div>
@@ -45,7 +52,9 @@
 							<p>
 								<i class="far fa-clock c-icon m__size-20 gm__txtColor-secondary"></i>
 								&nbsp;
-								<b class="gm__txtColor-primary">Your score: {{ timeLeft }} seconds</b>
+								<b class="gm__txtColor-primary">
+									Your score: {{ timeLeft != null ? timeLeft : 25 }} seconds
+								</b>
 							</p>
 						</div>
 						<div class="c-flx m__flxRow gu__flx-between-center">
@@ -90,6 +99,15 @@
 				</div>
 			</div>
 		</div>
+		<div class="c-holder gm__txtAlign-center gm__hidden:md">
+			<div class="c-flxInline m__flxColumn gu__flx-center">
+				<div class="x-txt xm__txtAlign-center">
+					<p>
+						<b>Your device is not compatible with this game</b>
+					</p>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -105,10 +123,13 @@
 		},
 		data() {
 			return {
+				landing: true,
 				playing: false,
 				name: "",
+				submited: false,
 				full: 0,
 				timeLeft: null,
+				extraTime: false,
 				dragged: null,
 			};
 		},
@@ -119,27 +140,45 @@
 		},
 		watch: {
 			timeLeft(newTime) {
-				if (this.playing) {
-					if (this.timeLeft > 0) {
-						// active game
-						let _this = this;
-						setTimeout(function() {
+				let _this = this;
+				setTimeout(function() {
+					if (_this.playing && _this.full < 5) {
+						if (_this.timeLeft > 0) {
+							// active game
 							_this.timeLeft = newTime - 1;
-						}, 1000);
-					} else {
-						// game finished
-						this.playing = false;
-						setTimeout(function() {
-							alert("Time out, you've lost");
-						}, 100);
+							if (_this.extraTime) {
+								_this.extraTime = false;
+								_this.timeLeft = newTime + 9;
+							} else {
+								_this.timeLeft = newTime - 1;
+							}
+						} else {
+							// game finished
+							_this.landing = true;
+							_this.playing = false;
+							setTimeout(function() {
+								//wait for mount, view change
+								alert("Time out, you've lost");
+							}, 100);
+							setTimeout(() => {
+								//restart automatically after 10s
+								_this.toGame();
+							}, 10000);
+						}
 					}
-				}
-				return;
+				}, 1000);
 			},
 			full(newAmount) {
+				let _this = this;
 				if (newAmount == 5 && this.playing) {
 					// You won
+					this.setListeners(false);
 					this.playing = false;
+					this.landing = true;
+					setTimeout(() => {
+						//restart automatically after 10s
+						_this.toGame();
+					}, 10000);
 				}
 			},
 		},
@@ -152,21 +191,33 @@
 			document.removeEventListener("keyup", this.keyUp, false);
 		},
 		methods: {
-			start: function() {
-				// star game if name available
+			toGame: function() {
 				if (this.name) {
-					this.playing = true;
-					this.full = 0;
-					this.timeLeft = 25; //25 by default
-					this.setListeners();
+					this.submited = true;
+					this.timeLeft = null;
+					this.landing = false;
+					let _this = this;
+					setTimeout(() => {
+						//wait for mount
+						_this.setListeners();
+					}, 100);
 					return;
 				}
 				return alert("Please provide a name before proceeding.");
 			},
+			start: function() {
+				// star game if name available
+				if (!this.playing) {
+					this.playing = true;
+					this.full = 0;
+					this.timeLeft = 25; //25 by default
+				}
+				return;
+			},
 			keyUp(e) {
 				if (e.key == "Enter" && !this.playing) {
 					//launch game on enter press
-					this.start();
+					this.toGame();
 				}
 			},
 			setListeners(set = true) {
@@ -194,6 +245,10 @@
 			},
 			isStart(e) {
 				//drag start
+				if (!this.playing) {
+					// start game
+					this.start();
+				}
 				this.dragged = e.target;
 			},
 			isOverToggle(e) {
@@ -207,33 +262,33 @@
 				this.isOverToggle(e);
 				if (e.target.classList.contains("gm__dropArea") && !e.target.classList.contains("is__full")) {
 					// remove from old element
-					if (e.target.dataset.symbol == this.dragged.dataset.symbol) {
-						let old = this.dragged.parentNode;
-						old.removeChild(this.dragged);
-						old.classList.remove("is__full");
-						old.setAttribute("disabled", true);
-						// add to new element
-						e.target.appendChild(this.dragged);
-						e.target.classList.add("is__full");
-						// check letter
+					let old = this.dragged.parentNode;
+					old.removeChild(this.dragged);
+					old.classList.remove("is__full");
+					// add to new element
+					e.target.appendChild(this.dragged);
+					e.target.classList.add("is__full");
+					if (e.target.dataset.symbol && e.target.dataset.symbol == this.dragged.dataset.symbol) {
+						// is correct
+						e.target.setAttribute("disabled", "");
 						this.full = this.full + 1;
+						if (old.dataset.base) {
+							old.setAttribute("disabled", "");
+						}
+					} else {
+						// is incorrect, add more time
+						if (!e.target.dataset.base) {
+							this.extraTime = true;
+						}
 					}
 				}
 			},
 			isAdvancedApi() {
 				// check support for drag and drop
 				var div = document.createElement("div");
-				return (
-					("draggable" in div || ("ondragstart" in div && "ondrop" in div)) &&
-					"FormData" in window &&
-					"FileReader" in window
-				);
+				return "draggable" in div || ("ondragstart" in div && "ondrop" in div);
 			},
 		},
 		// TERMINA LIFEHOOKS
 	};
 </script>
-
-<style lang="scss">
-	@import "_scss/00-utils/module.scss";
-</style>
